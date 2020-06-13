628e03e278aab924165bff0f431dc5e1eb885dd4
Bryan Gomes <bryanpgomes@gmail.com>
2020-06-13 01:28:29 +0000

    Added numberOfOffspring & numberOfVampiresFromOriginal

--- vampire.js
+++ vampire.js
@@ -16,12 +16,18 @@ class Vampire {
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let numberOfVampires = 0;
+    let currentVampire = this;
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
